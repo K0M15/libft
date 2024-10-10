b79a2a414870d601c345810b33baf8135f87f65a
Alain Felger <alain.felger93@googlemail.com>
2024-10-10 08:20:28 +0200

    rm bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:30:17 by afelger           #+#    #+#             */
-/*   Updated: 2024/10/09 16:31:03 by afelger          ###   ########.fr       */
+/*   Updated: 2024/10/09 16:48:10 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,25 +75,5 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
-
-
-# ifdef FLAG_BONUS
-/*	ft_list.c	*/
-t_list			*ft_lstnew(void *content);
-int				ft_lstsize(t_list *lst);
-t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_front(t_list **lst, t_list *new);
-void			ft_lstadd_back(t_list **lst, t_list *new);
-
-/*	ft_list_crtl.c	*/
-void			ft_lstdelone(t_list *lst, void (*del)(void*));
-void			ft_lstclear(t_list **lst, void (*del)(void*));
-
-/*	ft_list_apply.c	*/
-void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst,
-					void *(*f)(void *), void (*del)(void *));
-
-# endif /* FLAG_BONUS */
 
 #endif /* LIBFT_H */
