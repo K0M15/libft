86ad6c167d1ef16dd9e2dff683ee4dd293814449
Alain Felger <alain.felger93@googlemail.com>
2024-10-15 16:15:45 +0200

    working bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 16:09:33 by afelger           #+#    #+#             */
-/*   Updated: 2024/10/15 10:04:10 by afelger          ###   ########.fr       */
+/*   Updated: 2024/10/15 15:33:35 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, unsigned int length)
+void	ft_bzero(void *s, unsigned long length)
 {
 	unsigned long	c;
 
@@ -20,5 +20,6 @@ void	ft_bzero(void *s, unsigned int length)
 	while (c < length)
 	{
 		((char *)s)[c] = 0;
+		c++;
 	}
 }
