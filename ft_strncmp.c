28290b7b3c114bc844bb07cf40dd05a3a3c26aea
Alain Felger <alain.felger93@googlemail.com>
2024-10-15 17:03:17 +0200

    working strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:14:18 by afelger           #+#    #+#             */
-/*   Updated: 2024/10/15 14:15:01 by afelger          ###   ########.fr       */
+/*   Updated: 2024/10/15 17:02:35 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 unsigned long	ft_strncmp(const char *s1, const char *s2, unsigned long n)
 {
-	unsigned long	counter;
+	unsigned long		counter;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
+	str1 = (const unsigned char *) s1;
+	str2 = (const unsigned char *) s2;
 	counter = 0;
-	while (counter < n && s1[counter] - s2[counter] == 0 && s1[counter])
+	if (n == 0)
+		return (0);
+	while (counter + 1 < n && str1[counter] - str2[counter] == 0)
 		counter++;
-	return ((unsigned char) s1[counter] - (unsigned char) s2[counter]);
+	return (str1[counter] - str2[counter]);
 }
