499680ad818bd0c440dfb4f958b603c6c922805d
Alain Felger <alain.felger93@googlemail.com>
2024-10-15 16:22:29 +0200

    working memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:20:46 by afelger           #+#    #+#             */
-/*   Updated: 2024/10/15 14:54:31 by afelger          ###   ########.fr       */
+/*   Updated: 2024/10/15 16:21:49 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, unsigned long n)
 {
-	return (ft_strncmp((const char *)s1, (const char *)s2, n));
+	unsigned long		counter;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	counter = 0;
+	if (n == 0)
+		return (0);
+	while (counter + 1 < n && (str1[counter] - str2[counter]) == 0)
+		counter++;
+	return (str1[counter] - str2[counter]);
 }
