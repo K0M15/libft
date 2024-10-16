8ac488eaded988dfa19ecf6b1efb46a427c68322
Alain Felger <alain.felger93@googlemail.com>
2024-10-16 14:33:25 +0200

    working strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 16:21:02 by afelger           #+#    #+#             */
-/*   Updated: 2024/10/15 14:56:34 by afelger          ###   ########.fr       */
+/*   Updated: 2024/10/16 14:32:15 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,21 @@ unsigned long	ft_strlcat(char *dst, const char *src, unsigned long dsize)
 {
 	unsigned long	src_ctr;
 	unsigned long	dst_ctr;
+	unsigned long	result;
 
 	src_ctr = 0;
 	dst_ctr = ft_strlen(dst);
-	while (dst_ctr < dsize && src[src_ctr])
+	if (dst_ctr < dsize)
+		result = dst_ctr;
+	else
+		result = dsize;
+	while (dst_ctr + 1 < dsize && src[src_ctr] && dsize)
 	{
 		dst[dst_ctr] = src[src_ctr];
 		dst_ctr++;
 		src_ctr++;
 	}
-	dst[dst_ctr] = 0;
-	return (dsize);
+	if (dst_ctr < dsize)
+		dst[dst_ctr] = 0;
+	return (result + ft_strlen(src));
 }
